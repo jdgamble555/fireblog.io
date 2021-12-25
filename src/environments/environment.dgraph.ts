11f91dcedb25980f792a69e9dfd7f47d22446703
Jonathan Gamble <jdgamble555@gmail.com>
2021-12-25 13:42:28 -0600

    test

---
+++ src/environments/environment.dgraph.ts
@@ -0,0 +1,8 @@
+import * as firebase_env from './environment.dgraph.json';
+
+export const environment = {
+  production: false,
+  dgraph: true,
+  firebase: firebase_env
+};
+
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import * as firebase_env from './environment.prod.json';
+
+export const environment = {
+  production: true,
+  firebase: firebase_env
+};
