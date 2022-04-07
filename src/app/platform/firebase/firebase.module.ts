2d3a5d85b3940bf19df56c1b196707e3105d87a3
Jonathan Gamble <jdgamble555@gmail.com>
2022-04-06 22:47:36 -0500

    test

--- src/app/platform/firebase/firebase.module.ts
+++ src/app/platform/firebase/firebase.module.ts
@@ -3,7 +3,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-//import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
 import {
   getAnalytics,
   provideAnalytics,
