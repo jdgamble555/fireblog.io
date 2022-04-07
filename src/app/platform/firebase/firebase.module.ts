5ad64dfaf7e2db064c028edab1d5e43ab9431a86
Jonathan Gamble <jdgamble555@gmail.com>
2022-04-07 00:22:16 -0500

    test

--- src/app/platform/firebase/firebase.module.ts
+++ src/app/platform/firebase/firebase.module.ts
@@ -22,6 +22,7 @@ const FirebaseEVN = (state: StateService): any => {
     }
   } else if (state.hasState('fb')) {
     const fb = state.getState('fb');
+    console.log(fb);
     return fb;
   }
 };
