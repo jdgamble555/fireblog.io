40d5288ef0d1f462f27521e47ac8059ef8c3f80f
Jonathan Gamble <jdgamble555@gmail.com>
2022-04-06 21:44:39 -0500

    vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-import * as firebase_env from './environment.prod.json';
+//import * as firebase_env from './environment.prod.json';
 
 export const environment = {
   production: true,
-  firebase: firebase_env
+  //firebase: firebase_env
 };
