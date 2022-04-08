50dbda92d28cc0da6fb62e1b5b075a9791fef579
Jonathan Gamble <jdgamble555@gmail.com>
2022-04-07 21:57:20 -0500

    tes

--- src/environments/set-env.js
+++ src/environments/set-env.js
@@ -23,4 +23,30 @@ function setEnv() {
   });
 }
 
+function setEnv2() {
+  fs = require("fs");
+  writeFile = fs.writeFile;
+  // Configure Angular `environment.prod.json` file path
+  targetPath = "/vercel/path2/src/environments/environment.prod.json";
+
+  // `environment.prod.json` file structure
+  envConfigFile = JSON.stringify(JSON.parse(process.env.FIREBASE));
+
+
+  console.log(
+    "The file `environment.prod.json` will be written with the following content: \n"
+  );
+  writeFile(targetPath, envConfigFile, function (err) {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(
+        "Angular environment.prod.json file generated correctly at" + targetPath + "\n"
+      );
+    }
+  });
+}
+
 setEnv();
+setEnv2();
